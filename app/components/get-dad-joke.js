b6095d56f19440f3ce9f1eb8cdad7b1299a000c1
Jim Schofield <oldcoyote@gmail.com>
2022-03-06 14:03:48 -0600

    0-component-driven-fetch

--- app/components/get-dad-joke.js
+++ app/components/get-dad-joke.js
@@ -3,13 +3,29 @@ import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class GetDadJokeComponent extends Component {
-  @service('dad-joke') dadJokeService;
-
   @tracked joke;
+  @tracked error;
 
   constructor() {
     super(...arguments);
 
-    this.dadJokeService.fetchDadJoke().then((joke) => (this.joke = joke));
+    this.fetchDadJoke();
+  }
+
+  async fetchDadJoke() {
+    try {
+      const res = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+          accept: 'application/json',
+          'User-Agent': 'ember-demonstration',
+        },
+      });
+
+      const data = await res.json();
+
+      this.joke = data.joke;
+    } catch (error) {
+      this.error = error;
+    }
   }
 }
