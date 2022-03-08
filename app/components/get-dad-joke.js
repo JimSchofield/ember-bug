19fad692b41166e37daefe392388844c80682151
Jim Schofield <oldcoyote@gmail.com>
2022-03-08 15:27:27 -0600

    Issue with ember-resources ?

--- app/components/get-dad-joke.js
+++ app/components/get-dad-joke.js
@@ -1,34 +1,23 @@
 import Component from '@glimmer/component';
-import { task } from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
-
-async function getRandomWord() {
-  const response = await fetch('https://random-words-api.herokuapp.com/w?n=1');
-
-  const data = await response.json();
-
-  await new Promise((res) => {
-    setTimeout(() => res(), 2000);
-  });
-
-  if (Math.random() < 0.5) {
-    return data[0];
-  } else {
-    return Promise.reject('Your request is denied!');
-  }
-}
+import { useTask } from 'ember-resources';
 
 export default class GetDadJokeComponent extends Component {
   @tracked data;
 
-  constructor() {
-    super(...arguments);
-
-    this.getRandomWord.perform();
-  }
+  word = useTask(this, this.getRandomWord);
 
   @task
   *getRandomWord() {
-    this.data = yield getRandomWord();
+    yield timeout(2000);
+
+    const response = yield fetch(
+      'https://random-words-api.herokuapp.com/w?n=1'
+    );
+
+    const data = yield response.json();
+
+    return data[0];
   }
 }
