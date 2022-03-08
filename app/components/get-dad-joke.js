d2cf396bb46a9e4f39b0b2ce05a26b3375cdcea0
Jim Schofield <oldcoyote@gmail.com>
2022-03-07 18:55:33 -0600

    Working tracked function

--- app/components/get-dad-joke.js
+++ app/components/get-dad-joke.js
@@ -1,30 +1,20 @@
 import Component from '@glimmer/component';
-import FetchHandler from '../util/fetch-handler';
-import { computed } from '@ember/object';
+import { trackedFunction } from 'ember-resources';
 
 export default class GetDadJokeComponent extends Component {
-  @computed('args.searchText')
-  get apiHandler() {
-    return new FetchHandler(
+  joke = trackedFunction(this, async () => {
+    const res = await fetch(
       `https://icanhazdadjoke.com/search?term=${this.args.searchText}`,
       {
         headers: {
           accept: 'application/json',
           'User-Agent': 'ember-demonstration',
         },
-      },
-      true
+      }
     );
-  }
 
-  get joke() {
-    // for the example, just get the first result :D
-    return this.apiHandler.data
-      ? this.apiHandler.data.results[0]?.joke
-      : undefined;
-  }
+    const data = await res.json();
 
-  get error() {
-    return this.apiHandler ? this.apiHandler.error : undefined;
-  }
+    return data?.results[0].joke ?? 'No joke found!';
+  });
 }
