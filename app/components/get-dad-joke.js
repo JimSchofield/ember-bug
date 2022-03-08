09043f314fb10102487b67928913f35743054e67
Jim Schofield <oldcoyote@gmail.com>
2022-03-07 19:14:52 -0600

    Coordinating two tracked functions

--- app/components/get-dad-joke.js
+++ app/components/get-dad-joke.js
@@ -1,10 +1,13 @@
 import Component from '@glimmer/component';
 import { trackedFunction } from 'ember-resources';
+import { tracked } from '@glimmer/tracking';
 
 export default class GetDadJokeComponent extends Component {
+  @tracked error;
+
   joke = trackedFunction(this, async () => {
     const res = await fetch(
-      `https://icanhazdadjoke.com/search?term=${this.args.searchText}`,
+      `https://icanhazdadjoke.com/search?term=${this.randomWord.value}`,
       {
         headers: {
           accept: 'application/json',
@@ -15,6 +18,18 @@ export default class GetDadJokeComponent extends Component {
 
     const data = await res.json();
 
-    return data?.results[0].joke ?? 'No joke found!';
+    if (data.results.length > 0) {
+      return data.results[0].joke;
+    } else {
+      return 'No joke found!';
+    }
+  });
+
+  randomWord = trackedFunction(this, async () => {
+    const res = await fetch(`https://random-words-api.herokuapp.com/w?n=1`);
+
+    const data = await res.json();
+
+    return data[0];
   });
 }
