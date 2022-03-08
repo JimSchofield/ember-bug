d5f712339fd5340e571c4d3e528f5d7a9e7dd285
Jim Schofield <oldcoyote@gmail.com>
2022-03-08 15:18:26 -0600

    Using ember concurrency niceties

--- app/components/get-dad-joke.js
+++ app/components/get-dad-joke.js
@@ -1,35 +1,34 @@
 import Component from '@glimmer/component';
-import { trackedFunction } from 'ember-resources';
+import { task } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
 
-export default class GetDadJokeComponent extends Component {
-  @tracked error;
+async function getRandomWord() {
+  const response = await fetch('https://random-words-api.herokuapp.com/w?n=1');
 
-  joke = trackedFunction(this, async () => {
-    const res = await fetch(
-      `https://icanhazdadjoke.com/search?term=${this.randomWord.value}`,
-      {
-        headers: {
-          accept: 'application/json',
-          'User-Agent': 'ember-demonstration',
-        },
-      }
-    );
+  const data = await response.json();
 
-    const data = await res.json();
-
-    if (data.results.length > 0) {
-      return data.results[0].joke;
-    } else {
-      return 'No joke found!';
-    }
+  await new Promise((res) => {
+    setTimeout(() => res(), 2000);
   });
 
-  randomWord = trackedFunction(this, async () => {
-    const res = await fetch(`https://random-words-api.herokuapp.com/w?n=1`);
-
-    const data = await res.json();
-
+  if (Math.random() < 0.5) {
     return data[0];
-  });
+  } else {
+    return Promise.reject('Your request is denied!');
+  }
+}
+
+export default class GetDadJokeComponent extends Component {
+  @tracked data;
+
+  constructor() {
+    super(...arguments);
+
+    this.getRandomWord.perform();
+  }
+
+  @task
+  *getRandomWord() {
+    this.data = yield getRandomWord();
+  }
 }
